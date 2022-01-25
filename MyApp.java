b0becda07677607a3b0adeca22986c9aa6c29796
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2022-01-25 19:48:02 +0530

    Session 1

---
+++ MyApp.java
@@ -0,0 +1,10 @@
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first JAVA Program");
+		System.out.println("This is my first time using Eclipse IDE");
+
+	}
+
+}
